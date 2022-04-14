32b281c17cec2bd39e2ed857dcdc7e8c9477812b
flori <flori@192.168.1.21>
2022-04-14 09:23:13 +0200

    Update Servlet

--- project-jee2/src/main/java/fr/formation/inti/controllers/IndexController.java
+++ project-jee2/src/main/java/fr/formation/inti/controllers/IndexController.java
@@ -1,6 +1,8 @@
 package fr.formation.inti.controllers;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,15 +21,15 @@ public class IndexController extends HttpServlet {
      */
     public IndexController() {
         super();
-        // TODO Auto-generated constructor stub
+      
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out = response.getWriter();
+		
 	}
 
 	/**
